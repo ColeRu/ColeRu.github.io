04e8039456ccd01a00739b228aa965ef7ae6876d
ColeRu <60522893+ColeRu@users.noreply.github.com>
2020-12-03 12:13:12 -0500

    Update final.py

--- final.py
+++ final.py
@@ -1,7 +1,13 @@
-#ideas
 
-#age groups
-#race
-#sex
-#income (if available in data)
-#
+#import and tidy data
+    #after this we will prob have a better idea for EDV and ML applications
+
+#FOR EDA/EDV FIGURES
+    #age groups
+    #race
+    #sex
+    #income (if available in data)
+
+
+#ML APPLICATIONS
+    #
